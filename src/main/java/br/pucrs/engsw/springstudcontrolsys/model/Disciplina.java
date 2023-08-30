03f77a54522bad4bae3e9065732b7aa066a7dcbc
mdmmora <michael.c.mora@gmail.com>
2023-08-30 18:15:36 -0300

    Ajustando para início do tema em 2023-2

--- src/main/java/br/pucrs/engsw/springstudcontrolsys/model/Disciplina.java
+++ src/main/java/br/pucrs/engsw/springstudcontrolsys/model/Disciplina.java
@@ -2,8 +2,6 @@ package br.pucrs.engsw.springstudcontrolsys.model;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
